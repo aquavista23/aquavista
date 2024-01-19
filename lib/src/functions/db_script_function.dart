6f27b62b18bc5629446aa996374c321cd1cd807a
aquavista00 <aquavista00@gmail.com>
2024-01-18 20:21:16 -0400

    actualizar COMPARTIR listo

--- lib/src/functions/db_script_function.dart
+++ lib/src/functions/db_script_function.dart
@@ -1,9 +1,13 @@
+// ignore_for_file: avoid_print
+
 import 'package:aquavista/src/models/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 Future<bool> updateUser(CollectionReference userColection,
     DocumentSnapshot documentSnapshot, User currentUser, String? token) async {
+  List<String> listID = [];
+  Map mapShared = {};
   try {
     UserData? oldToken = UserData.fromJson(documentSnapshot.data() as Map);
     if (oldToken.token != token) {
@@ -12,6 +16,28 @@ Future<bool> updateUser(CollectionReference userColection,
           .doc(currentUser.uid)
           .update({'fecha_token': DateTime.now().toString()});
     }
+    await userColection.doc(currentUser.uid).get().then((querySnapshot) async {
+      if (querySnapshot.exists) {
+        Map shared = querySnapshot.data() as Map;
+
+        listID.addAll(shared['compartir'].keys);
+        print('???????????????????????????//1 $listID');
+
+        for (var v = 0; v < listID.length; v++) {
+          await userColection.doc(listID[v]).get().then((sharedSnapshot) async {
+            print('???????????????????????????//2 $sharedSnapshot');
+
+            if (sharedSnapshot.exists) {
+              mapShared[listID[v]] = sharedSnapshot['token'];
+            }
+          });
+        }
+        print('???????????????????????????//3 $mapShared');
+        await userColection
+            .doc(currentUser.uid)
+            .update({'compartir': mapShared});
+      }
+    });
     return true;
   } catch (e) {
     print('??????????????//update ${e.toString()}');
@@ -36,5 +62,14 @@ Future<bool> createUser(String id, String email, String? token) async {
   } catch (e) {
     print('??????????????//create ${e.toString()}');
     return false;
+  }
+}
+
+Future<Map> updateShared(CollectionReference userColection, String id) async {
+  try {
+    return {};
+  } catch (e) {
+    print('??????????????//updateShared ${e.toString()}');
+    return {};
   }
 }
